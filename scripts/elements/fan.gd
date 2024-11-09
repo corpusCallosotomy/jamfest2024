90985fdcff28e6cd9ede9f4a65b2d15424e7bc0d
Max <max.fowlkes@gmail.com>
2024-11-08 22:25:35 -0500

    added sliders, sauce

--- scripts/elements/fan.gd
+++ scripts/elements/fan.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 @export var fanForce : int = 100
 
+@export var minimumPoint : float = self.position.y-167.5
+@export var maximumPoint : float = self.position.y+167.5
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Area2D.position.y = maximumPoint + (minimumPoint-maximumPoint) * $VSlider.value/100
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,3 +15,8 @@ func _physics_process(delta):
 	for body in $Area2D.get_overlapping_bodies():
 		if body.is_in_group("ball"):
 			body.apply_central_impulse(self.transform.x * fanForce * delta)
+
+
+func _on_v_slider_value_changed(value):
+	# get number value% between minimum and maximum
+	$Area2D.position.y = maximumPoint + (minimumPoint-maximumPoint) * value/100
