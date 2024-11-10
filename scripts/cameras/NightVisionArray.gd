ac29b2c54452fd4b8bca3bf699c8397eb56d4d1d
Max <max.fowlkes@gmail.com>
2024-11-10 17:58:28 -0500

    life is a nightmare
    we have not fixed the bug but did build the nva from the ground up

--- scripts/cameras/NightVisionArray.gd
+++ scripts/cameras/NightVisionArray.gd
@@ -1,14 +1,26 @@
 extends Node2D
 
+var scaleFactor : float
+
 var startingPosition : Vector2 = Vector2(300,300)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	$SubViewport.set_world_2d(get_tree().root.get_world_2d())
+
+
+	scaleFactor = (float(DisplayServer.screen_get_size().x) / 1152)	
+	$NightVision.size *= scaleFactor
+	$SubViewport.size*=scaleFactor
+	$SubViewport/Camera2D.zoom*=scaleFactor
+	$NightVision/ColorRect.size*=scaleFactor
+	$NightVision/CamOverlay.scale*=scaleFactor
+
+	
 	$SubViewport/Camera2D.pause()
 	await get_tree().create_timer(.5).timeout
-	$SubViewport.subViewportBootup()
-	
-	
+	#$SubViewport.subViewportBootup()
 	$SubViewport/Camera2D.startingPosition=startingPosition
 	$SubViewport/Camera2D.unpause()
 	$SubViewport/Camera2D.visible=true
@@ -17,6 +29,8 @@ func _ready():
 	
 	setupViewportTexture()
 	
+
+
 
 func setupViewportTexture():
 	$NightVision/Sprite2D.texture = $SubViewport.get_texture()
